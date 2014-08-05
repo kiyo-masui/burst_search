8dcd58ef5b41a3eb099cb67deea7b3fb4217532c
Kiyoshi Masui <kiyo@physics.ubc.ca>
2014-08-04 22:27:26 -0700

    Function prototypes for C dedispersion code.

---
+++ src/dedisperse.c
@@ -0,0 +1,31 @@
+/*
+ * As far as I can tell there is only one parameter in Jon's code, a *depth*
+ * parameter in `map_chans`.  *delta_t* and *max_dm* are only required to
+ * interpret `depth`. I may be completely wrong on this. -km
+ *
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <omp.h>
+
+
+// For allocating output buffer.
+size_t get_num_dispersions(size_t nfreq, float freq0,
+                           float delta_f, int depth);
+
+// Return minimum *depth* parameter required to achieve given maximum DM.
+int depth_for_max_dm(float max_dm, float delta_t, size_t nfreq, 
+                     float freq0, float delta_f, int depth);
+
+// *ntime2* is allowed to be 0.  Return number of valid dedispersed time samples,
+// (always less than *ntime1*).
+size_t dedisperse(float *indata1, float *indata2, float *outdata,
+                  size_t ntime1, size_t ntime2, float delta_t,
+                  size_t nfreq, float freq0, float delta_f, int depth);
