7c4a8f31a6cff6ce115df005db6cc5dcf2168724
Kiyoshi Masui <kiyo@physics.ubc.ca>
2014-08-06 11:22:18 -0700

    Started guppi IO and drivers.

--- burst_search/guppi.py
+++ burst_search/guppi.py
@@ -1,9 +1,48 @@
-"""Driver scripts for Green Bank GUPPI data.
+"""Driver scripts and IO for Greenbank GUPPI data.
 
 """
 
-def read_records(filename, start_record=0, end_record=None):
+
+import pyfits
+
+
+def search_file(filename):
+    """Simple dirver function to search a GUPPI file."""
+
+    hdulist = pyfits.open(filename, 'readonly')
+    
+    parameters = parameters_from_header(hdulist)
+
+    hdulist.close()
+
+
+def parameters_from_header(hdulist):
+    """Get data acqusition parameters for psrfits file header.
+
+    Returns
+    -------
+    parameters : dict
+
+    """
+
+    parameters = {}
+
+    print repr(hdulist[0].header)
+
+    # XXX For now just fake it.
+    parameters['cal_period'] = 64
+    parameters['delta_t'] = 0.001
+    parameters['nfreq'] = 4096
+    parameters['freq0'] = 900e6
+    parameters['delta_f'] = -200e6 / 4096
+
+    return parameters
+
+
+def read_records(hdulist, start_record=0, end_record=None):
     """Read and format records from GUPPI PSRFITS file."""
+
+
 
 
 # This wants to be a class for sure.
