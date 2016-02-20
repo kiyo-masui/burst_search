7fb6f098adae4feb6b3d4ee1729c4fc5a8087688
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n004-ib0.(none)>
2016-02-20 14:47:24 -0500

    reduce width of preprocess.highpass_filter in guppi.py

--- burst_search/guppi.py
+++ burst_search/guppi.py
@@ -427,7 +427,7 @@ class FileSearch(object):
 
             # 200 ms (hard coded) highpass filter
             preprocess.remove_outliers(data, 5, 512)
-            data = preprocess.highpass_filter(data, 0.200 / parameters['delta_t'])
+            data = preprocess.highpass_filter(data, 0.100 / parameters['delta_t'])
 
             preprocess.remove_outliers(data, 5)
             preprocess.remove_noisy_freq(data, 3)
